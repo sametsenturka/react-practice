3f05e66c299ee6cb513a72c7224c14b455204c07
Samet Senturk <sametsenturk035@gmail.com>
2025-04-18 19:43:59 +0300

    Update UserProfile.jsx

--- UserCRUD-react/UserProfile.jsx
+++ UserCRUD-react/UserProfile.jsx
@@ -8,7 +8,7 @@ const UserProfile = () => {
     <div>
       <h2>User Profile</h2>
       <p>Name: {user.name}</p>
-      <p>Email: {user.email}</p>
+      <p>Email: {user.name}@gmail.com</p>
     </div>
   );
 };
